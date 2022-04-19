2e3cba5b5c42acf38b77c526e043ff7ab9eec79e
inoveAlumnos <alumnos@inove.com.ar>
2022-04-19 09:48:13 -0300

    Ejercicios de practica

---
+++ ejercicios_practica/1- eventos/js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas y poner
+ * en práctica lo visto en clase
+ */
+
+/* 1 - Enunciado
+
+Transforme todos los eventos a continuación a eventos con on-event
+Verifique que en consola no figura ningún error en alguno de ellos
+y continue funcionando con el boton del HTML
+
+*/
+
+const boton = document.querySelector("button");
+
+boton.addEventListener("click", () => {
+    console.log("Respuesta evento click");
+    boton.className = "btnClick";
+})
+
+boton.addEventListener("mouseover", () => {
+    console.log("Respuesta evento mouseover");
+    boton.className = "btnOver";
+})
+
+boton.addEventListener("mouseout", () => {
+    console.log("Respuesta evento mouseout");
+    boton.className = "btnOut";
+})
+
+
+/* 2 - Enunciado
+
+Cree un evento que capture cuando cambia la caja de seleccion
+Utilice el ejemplo de clase de modo "white y black" para guiarse
+en como capturar este evento y además como capturar el valor
+actual seleccionado en la caja de seleccion.
+
+*/
+
+/* 3 - Enunciado
+
+Cree un evento que capture cuando haya finalizado de ingresas
+su nombre en el elemento "input". Deberá con un alert informar
+el nombre ingresado al finalizar su ingreso.
+PISTA: Debe utilizar el mismo tipo de evento que en el ejercicio
+anterior.
+
+*/
